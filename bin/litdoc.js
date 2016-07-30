9adf022584345e5b0c39af60818acada4a2409d3
Bryan Helmig <bryan@bryanhelmig.com>
2016-07-30 16:40:39 -0700

    also render down for command line

--- bin/litdoc.js
+++ bin/litdoc.js
@@ -1,16 +1,18 @@
 #!/usr/bin/env node
-const path = require('path');
-const generate = require('../index');
+'use strict';
 
-const args = process.argv.slice(2);
+var path = require('path');
+var generate = require('../index');
+
+var args = process.argv.slice(2);
 
 if (args.length !== 2) {
   console.log('Only two arguments allowed:\n\n  litdoc input.md output.html');
 } else {
-  const markdownPath = path.join(process.cwd(), args[0]);
-  const outputPath = path.join(process.cwd(), args[1]);
-  console.log(`Reading "${markdownPath}"...`);
-  console.log(`Writing "${outputPath}"...`);
+  var markdownPath = path.join(process.cwd(), args[0]);
+  var outputPath = path.join(process.cwd(), args[1]);
+  console.log('Reading "' + markdownPath + '"...');
+  console.log('Writing "' + outputPath + '"...');
   generate({
     markdownPath: markdownPath,
     outputPath: outputPath
